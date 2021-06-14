0da77344ea10103023b0d60713b0a0b39f98c128
zerosum <zhen_87@naver.com>
2021-06-14 16:57:20 +0900

    Update gatsby-node.js

--- gatsby-node.js
+++ gatsby-node.js
@@ -29,13 +29,51 @@ exports.createPages = ({ graphql, actions }) => {
     })
   })
 }
-exports.sourceNodes = ({ actions }) => {
+exports.createSchemaCustomization = ({ actions }) => {
   const { createTypes } = actions
   const typeDefs = `
-    type AuthorJson implements Node {
-      name: String
-      birthday: Date
+    interface TeamMember {
+      name: String!
+      firstName: String!
+      email: String!
+    }
+    type AuthorJson implements Node & TeamMember {
+      name: String!
+      firstName: String!
+      email: String!
+      joinedAt: Date
+    }
+    type ContributorJson implements Node & TeamMember {
+      name: String!
+      firstName: String!
+      email: String!
+      receivedSwag: Boolean
     }
   `
   createTypes(typeDefs)
+}
+exports.createResolvers = ({ createResolvers }) => {
+  const fullName = {
+    type: "String",
+    resolve(source, args, context, info) {
+      return source.firstName + " " + source.name
+    },
+  }
+  const resolvers = {
+    Query: {
+      allTeamMembers: {
+        type: ["TeamMember"],
+        resolve(source, args, context, info) {
+          return context.nodeModel.getAllNodes({ type: "TeamMember" })
+        },
+      },
+    },
+    AuthorJson: {
+      fullName,
+    },
+    ContributorJson: {
+      fullName,
+    },
+  }
+  createResolvers(resolvers)
 }
