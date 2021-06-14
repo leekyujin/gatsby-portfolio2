4e1980d90e494c78b938f9d244dbf247a1dd1330
zerosum <zhen_87@naver.com>
2021-06-14 16:50:19 +0900

    Update gatsby-node.js

--- gatsby-node.js
+++ gatsby-node.js
@@ -29,3 +29,12 @@ exports.createPages = ({ graphql, actions }) => {
     })
   })
 }
+exports.createSchemaCustomization = ({ actions }) => {
+  const { createTypes } = actions
+  const typeDefs = `
+    type AuthorJson implements Node {
+      joinedAt: Date
+    }
+  `
+  createTypes(typeDefs)
+}
